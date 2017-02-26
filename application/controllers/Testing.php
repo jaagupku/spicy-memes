e73a3f13b059d1d9913f0bff7b752b04c0e07577
h-mj <hj@online.ee>
2017-02-26 16:15:43 +0200

    test 123

--- application/controllers/Testing.php
+++ application/controllers/Testing.php
@@ -1,15 +1,20 @@
 <?php
-class Testing extends CI_Controller {
 
-        public function view($page = 'test')
-        {
-			if ( ! file_exists(APPPATH.'views/test/'.$page.'.php'))
-			{
-					// Whoops, we don't have a page for that!
-					show_404();
-			}
+class Testing extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->helper('url');
+        redirect('/testing/view', 'test');
+    }
 
-			$this->load->view('test/'.$page);
+    public function view($page = 'test')
+    {
+        if (!file_exists(APPPATH . 'views/test/' . $page . '.php')) {
+            // Whoops, we don't have a page for that!
+            show_404();
         }
+
+        $this->load->view('test/' . $page);
+    }
 }
-?>
