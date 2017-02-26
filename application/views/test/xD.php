1a7a6d615c9c5bc3d5c76e13a8b2c46da14e6866
h-mj <hj@online.ee>
2017-02-26 19:03:26 +0200

    Lisasin rohkem xD-sid (#1)

---
+++ application/views/test/xD.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <title>xD</title>
+    </head>
+    <body>
+
+    <?php for ($i = 0; $i < 100000; $i++) { ?> xD <?php } ?>
+
+    </body>
+</html>
