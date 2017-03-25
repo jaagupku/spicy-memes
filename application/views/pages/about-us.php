8e91308537a59a9f94dcca6d39bab93b3a82d4f9
Agu-Art <aguart@windowslive.com>
2017-03-25 22:34:13 +0200

    about page html+css

--- application/views/pages/about-us.php
+++ application/views/pages/about-us.php
@@ -7,8 +7,36 @@ array_push($scripts, '/assets/js/googlemap.js');
 array_push($scripts, 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDuSD8hPdFABJxJjQeS9HtCNQs08neegNg&callback=initMap'); // async defer
 ?>
 
-<h3>Our base of operations is located at:</h3>
-<div style="height: 400px; width: 100%;" id="map"></div>
+<div class="container-fluid"><div class="break"></div></div>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-xs-12 col-custom-about col-centered">
+      <h2>ABOUT US</h2>
+      <p>Spicy Memes is a website founded by three students from University of Tartu. The idea was to create an ultimate scrolling simulator for the spiciest memes ever made. Our team includes: Jaagup Kuhi, Henri-Martin Jaakson ja Agu-Art Annuk. </p>
+    </div>
+  </div>
+</div>
+
+<div class="container-fluid"><div class="break"></div></div>
+
+<div class="aboutus-location">
+  <h2>OUR BASE LOCATION</h2>
+  <div id="map"></div>
+</div>
+
+<div class="container-fluid"><div class="break"></div></div>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-xs-12 col-custom-about col-centered">
+      <h2>DONATIONS</h2>
+      <p>To help us boost our creativity and make everything more spicy, we have included a big red donate button. So everyone who thinks we deserve some recognition, hit that button. </p>
+      <div class="break"></div>
+      <a role="button" class="btn btn-lg" href="#">DONATE</a>
+    </div>
+  </div>
+</div>
 
 <?php
 include('footer.php');
--- application/views/pages/header.php
+++ application/views/pages/header.php
@@ -19,6 +19,7 @@ if (!isset($username)) {
     <link href="/assets/css/commentspagestyle.css" rel="stylesheet">
     <link href="/assets/css/frontpagebodystyle.css" rel="stylesheet">
     <link href="/assets/css/userpagestyle.css" rel="stylesheet">
+    <link href="/assets/css/aboutpagestyle.css" rel="stylesheet">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
   </head>
   <body>
