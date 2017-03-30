27c7feda20acaea76f5d19b0fad9383931521708
h-mj <hj@online.ee>
2017-03-30 08:15:30 +0300

    fixes #38 and scrollbar is not removed if login/singup modal opens

--- application/views/pages/header.php
+++ application/views/pages/header.php
@@ -62,8 +62,8 @@ if (!isset($username)) {
               <li <?php if($selection==='profile') {echo 'class="active"';} ?> ><a href="<?php echo site_url("profile/".$username) ?>" id="username"><span class="glyphicon glyphicon-user"></span> <?= $username ?></a></li>
               <li><a href="<?= site_url("logout") ?>" id="logout"><span class="glyphicon glyphicon-log-out"></span> LOG OUT</a></li>
             <?php } else { ?>
-              <li><a href="<?= site_url("register") ?>" role="button" class="btn btn-signup btn-md" data-toggle="modal" data-target="#signuploginmodal" data-remote="false"><span class="glyphicon glyphicon-user"></span> SIGN UP</a></li>
-              <li><a href="<?= site_url("login") ?>" role="button" class="btn btn-login btn-md" data-toggle="modal" data-target="#signuploginmodal" data-remote="false"><span class="glyphicon glyphicon-log-in"></span> LOG IN</a></li>
+              <li><a href="<?= site_url("register") ?>" role="button" class="btn-signup" data-toggle="modal" data-target="#signuploginmodal" data-remote="false"><span class="glyphicon glyphicon-user"></span> SIGN UP</a></li>
+              <li><a href="<?= site_url("login") ?>" role="button" class="btn-login" data-toggle="modal" data-target="#signuploginmodal" data-remote="false"><span class="glyphicon glyphicon-log-in"></span> LOG IN</a></li>
             <?php } ?>
           </ul>
 
