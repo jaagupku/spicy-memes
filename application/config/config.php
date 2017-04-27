c43b845d206bea58e1083b63dcf9cd38543404e0
h-mj <hj@online.ee>
2017-04-27 21:44:08 +0300

    oops

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 //$config['base_url'] = 'http://spicymemes.cs.ut.ee';
-$config['base_url'] = 'http://841d11eb.eu.ngrok.io/';
+$config['base_url'] = 'http://localhost';
 
 /*
 |--------------------------------------------------------------------------
