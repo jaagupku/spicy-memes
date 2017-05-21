f4e68189738ea910b121f9c36cb17cb36576ea91
h-mj <hj@online.ee>
2017-05-21 23:19:34 +0300

    update db structure

--- sql/spicymemes_structure.sql
+++ sql/spicymemes_structure.sql
@@ -226,7 +226,8 @@ CREATE TABLE `users` (
   `ProfileImg_Id` varchar(100) COLLATE utf8_bin NOT NULL DEFAULT 'noprofileimg.jpg',
   `mobile_number` varchar(15) COLLATE utf8_bin DEFAULT NULL,
   `Language` varchar(32) COLLATE utf8_bin NOT NULL DEFAULT 'english',
-  `FB_Id` varchar(64) COLLATE utf8_bin DEFAULT NULL
+  `FB_Id` varchar(64) COLLATE utf8_bin DEFAULT NULL,
+  `confirmation` varchar(32) COLLATE utf8_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
