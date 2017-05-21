a6ef92a38c4caeed832f7d5697b92a57bf0699c6
h-mj <hj@online.ee>
2017-05-21 23:39:12 +0300

    moved email.php to configs folder

---
+++ configs/email.php
@@ -0,0 +1,10 @@
+<?php
+
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = '';
+$config['smtp_port'] = 465;
+$config['smtp_user'] = '';
+$config['smtp_pass'] = '';
+$config['mailtype'] = 'html';
+$config['charset'] = 'utf-8';
+$config['from'] = '';
--- public_html/application/config/email.php
+++ public_html/application/config/email.php
@@ -1,10 +1,10 @@
 <?php
 
 $config['protocol'] = 'smtp';
-$config['smtp_host'] = '';
+$config['smtp_host'] = 'ssl://spicymemes.cs.ut.ee';
 $config['smtp_port'] = 465;
-$config['smtp_user'] = '';
-$config['smtp_pass'] = '';
+$config['smtp_user'] = 'noreply@spicymemes.cs.ut.ee';
+$config['smtp_pass'] = 'Tdg(zi4]UJoq';
 $config['mailtype'] = 'html';
 $config['charset'] = 'utf-8';
-$config['from'] = '';
+$config['from'] = 'noreply@spicymemes.cs.ut.ee';
